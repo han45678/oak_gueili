69ecb61655db162b0a14c8685467980e40f78e72
molly8351858 <molly8358158@gmail.com>
2025-05-06 09:50:48 +0800

    修改

--- src/section/s1.vue
+++ src/section/s1.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="s1 w-full relative">
     <div class="video-container">
-      <iframe src="https://www.youtube.com/embed/vDaERgh6LW0?autoplay=1&mute=1&loop=1&playlist=vDaERgh6LW0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe src="https://www.youtube.com/embed/9MgTnkiSNTs?autoplay=1&mute=1&loop=1&playlist=9MgTnkiSNTs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   </section>
 </template>
